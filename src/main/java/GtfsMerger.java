aa10a53506f2974bbffb127d0d7603f878dfa09b
fabiott <ftanniguchi@gmail.com>
2019-06-17 12:54:18 -0300

    Fix

--- src/main/java/GtfsMerger.java
+++ src/main/java/GtfsMerger.java
@@ -35,7 +35,6 @@ public class GtfsMerger {
     private void mergeAgencies(GtfsDaoImpl priorityGtfs, GtfsDaoImpl otherGtfs){
         LOGGER.info("Merging agencies");
         for(Agency agency : priorityGtfs.getAllAgencies()){
-            agency.setId(PREFIX + agency.getId());
             agency.setTimezone(TIMEZONE);
             priorityGtfs.saveOrUpdateEntity(agency);
         }
